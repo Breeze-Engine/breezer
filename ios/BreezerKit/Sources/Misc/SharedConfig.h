da49b720408dd3a1dcbd501fb10ffa01c3f10218
krishpranav <krisna.pranav@gmail.com>
2023-09-16 13:46:51 +0530

    misc: sharedconfig

---
+++ ios/BreezerKit/Sources/Misc/SharedConfig.h
@@ -0,0 +1,17 @@
+//
+//  SharedConfig.h
+//  BreezerKit
+//
+//  Created by krisna pranav on 16/09/23.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface SharedConfig : NSObject
+
+NS_ASSUME_NONNULL_BEGIN
+
+@property(nonatomic, strong) NSString *applicationName;
+
+@end
